c54c723c6fb477ac5c87b83d32bca66dde89b883
Your Name <petesyoum@gmail.com>
2016-07-26 11:35:28 -0400

    LS lesson 2

--- calculator.rb
+++ calculator.rb
@@ -1,21 +1,25 @@
-# ask the user for two numbers
-# ask the user for an operation to perform
-# perform the operation on the two numbers
-# output the result
-
-#answer = Kernel.gets()
-#Kernel.puts(answer)
-
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
 def valid_number?(num)
-  num.to_i() != 0
+  num.to_i.to_s == num
+end
+
+def integer?(input)
+  input.to_i.to_s == input
+end
+
+def float?(input)
+  input.to_f.to_s == input
+end
+
+def number?(input)
+  integer?(input) || float?(input)
 end
 
 def operation_to_message(op)
-  case op
+  word = case op
   when '1'
     "Adding"
   when '2'
@@ -25,6 +29,10 @@ def operation_to_message(op)
   when '4'
     "Dividing"
   end
+
+  x = "A random line of code"
+
+  word
 end
 
 prompt("Welcome to the Calculator! Enter your name:")
@@ -42,14 +50,13 @@ end
 
 prompt("Hi #{name}!")
 
-loop do # main loop
-
-  number1 = '' # Must initialze the varibale outside of the loop b/c it is now inside the validation loop.
+loop do
+  number1 = ''
   loop do
     prompt("What's the First number?")
     number1 = Kernel.gets().chomp()
 
-    if valid_number?(number1)
+    if number?(number1)
       break
     else
       prompt("Hmm... that doesnt look like a valid number")
@@ -61,10 +68,10 @@ loop do # main loop
     prompt("What's the Second number?")
     number2 = Kernel.gets().chomp()
 
-    if valid_number?(number2)
+    if number?(number2)
       break
     else
-       prompt("Hmm... that doesn' look like a valid number")
+      prompt("Hmm... that doesn' look like a valid number")
     end
   end
 
@@ -80,8 +87,7 @@ loop do # main loop
 
   operator = ''
   loop do
-   operator = Kernel.gets().chomp()
-
+    operator = Kernel.gets().chomp()
     if %w(1 2 3 4).include?(operator)
       break
     else
@@ -90,21 +96,21 @@ loop do # main loop
   end
 
   prompt("#{operation_to_message(operator)} the two numbers...")
-  result = case operator #case statement
+  result = case operator # case statement
            when '1'
-            number1.to_i() + number2.to_i()
+             number1.to_i() + number2.to_i()
            when '2'
-            number1.to_i() - number2.to_i()
+             number1.to_i() - number2.to_i()
            when '3'
-            number1.to_i() * number2.to_i()
+             number1.to_i() * number2.to_i()
            when '4'
-            number1.to_f() / number2.to_f()
-  end
+             number1.to_f() / number2.to_f()
+           end
   prompt("The result is #{result}!")
 
   prompt("Do you want to perform another calculation? (Y to calculate again)")
   answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y') #break the loop UNLESS the answer STARTS-WITH (case-insensitive "Y")
+  break unless answer.downcase().start_with?('y')
 end
 
 prompt("Thank you for using the Calculator. Goodbye!")
